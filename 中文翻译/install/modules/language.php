9c6ba863640e09cebad4509aa49dcf18a38a2434
深圳市耀影科技有限公司 <web@yaoying.vip>
2023-04-11 02:30:28 +0800

    添加rukovoditel3.3.1得中文翻译文件

---
+++ 中文翻译/install/modules/language.php
@@ -0,0 +1,5 @@
+<h3>选择语言/Language/Язык</h3>
+<p><a href="index.php?step=checking_environment&lng=chinese"><img src="CN.png" bolder="0" style="vertical-align:middle">&nbsp;简体中文</a></p>
+<p><a href="index.php?step=checking_environment&lng=english"><img src="us.png" bolder="0" style="vertical-align:middle">&nbsp;English</a></p>
+<p><a href="index.php?step=checking_environment&lng=russian"><img src="ru.png" bolder="0" style="vertical-align:middle">&nbsp;Русский</a></p>
+<p><a href="index.php?step=checking_environment&lng=italian"><img src="it.png" bolder="0" style="vertical-align:middle">&nbsp;Italian</a></p>
